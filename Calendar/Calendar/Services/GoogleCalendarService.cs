a4874342d7e93d86843f7f49601270e0254c0677
Diego <diego.valdez.r@tecsup.edu.pe>
2023-07-04 23:19:23 -0500

    GoogleApi credentials

---
+++ Calendar/Calendar/Services/GoogleCalendarService.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Calendar.Services
+{
+    public class GoogleCalendarService
+    {
+        private readonly CalendarService calendarService;
+
+        public GoogleCalendarService()
+        {
+            var scopes = new List<string>
+            {
+                CalendarService.Scope.Calendar,
+                CalendarService.Scope.CalendarEvents,
+                CalendarService.Scope.CalendarEventsReadonly
+            };
+
+            var credentials = GoogleCredential.FromFile(@"D:\Users\diegu\Source\\Repos\Calendar\Calendar\Calendar\ViewModels\credentials.json")
+    .CreateScoped(scopes);
+
+
+            calendarService = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = "Calendar Sample"
+            });
+        }
+
+        public async Task<Event> CreateEventAsync(Event newEvent)
+        {
+            var request = calendarService.Events.Insert(newEvent, "primary");
+            return await request.ExecuteAsync();
+        }
+
+        public async Task<IList<Event>> GetUpcomingEventsAsync()
+        {
+            var request = calendarService.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            var events = await request.ExecuteAsync();
+            return events.Items;
+        }
+    }
+}
---
+++ Calendar/Calendar/ViewModels/EventViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Calendar.Services;
+using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calendar.ViewModels
+{
+    public class EventViewModel : INotifyPropertyChanged
+    {
+        private readonly GoogleCalendarService googleCalendarService;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ICommand CreateEventCommand { get; }
+
+        public EventViewModel()
+        {
+            googleCalendarService = new GoogleCalendarService();
+            CreateEventCommand = new Command(CreateEvent);
+        }
+
+        private async void CreateEvent()
+        {
+            var newEvent = new Event
+            {
+                Summary = Title,
+                Description = Description,
+                Start = new EventDateTime { DateTime = StartTime },
+                End = new EventDateTime { DateTime = EndTime }
+            };
+
+            try
+            {
+                var createdEvent = await googleCalendarService.CreateEventAsync(newEvent);
+                // Realizar cualquier acción necesaria después de la creación del evento
+                await Application.Current.MainPage.DisplayAlert("Success", "Event created successfully", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"Failed to create event: {ex.Message}", "OK");
+            }
+        }
+    }
+}
